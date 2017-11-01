8c2fddd45f8b165ee60310eb48c9085e1f101a0d
Robin Stumm <serverkorken@gmail.com>
2017-11-01 18:29:04 +0100

    fix tests against stubs

--- src/test/java/de/codazz/houseofcars/GarageTest.java
+++ src/test/java/de/codazz/houseofcars/GarageTest.java
@@ -3,27 +3,52 @@ package de.codazz.houseofcars;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /** @author rstumm2s */
 public class GarageTest {
-    Garage garage;
+    Garage emptyGarage;
 
     @Before
-    public void setUp() {}
+    public void setUp() {
+        emptyGarage = new Garage() {
+            @Override
+            public int numTotal() {
+                return 100;
+            }
+
+            @Override
+            public int numUsed() {
+                return 0;
+            }
+
+            @Override
+            public int numFree() {
+                return 100;
+            }
+
+            @Override
+            public Optional<Spot> nextFree() {
+                return Optional.of(new Spot() {});
+            }
+        };
+    }
 
     @Test
     public void numFree_empy_total() {
-        assertEquals(garage.numFree(), garage.numTotal());
+        assertEquals(emptyGarage.numFree(), emptyGarage.numTotal());
     }
 
     @Test
     public void numUsed_empty_0() {
-        assertEquals(garage.numUsed(), 0);
+        assertEquals(emptyGarage.numUsed(), 0);
     }
 
     @Test
     public void nextFree_empty_spot() {
-        assertTrue(garage.nextFree().isPresent());
+        assertTrue(emptyGarage.nextFree().isPresent());
     }
 }
--- src/test/java/de/codazz/houseofcars/PricingTest.java
+++ src/test/java/de/codazz/houseofcars/PricingTest.java
@@ -3,21 +3,29 @@ package de.codazz.houseofcars;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Duration;
 import java.time.Instant;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /** @author rstumm2s */
 public class PricingTest {
     Pricing pricing;
 
     @Before
-    public void setUp() throws Exception {}
+    public void setUp() throws Exception {
+        pricing = new AbstractPricing() {
+            @Override
+            public int euroCents(final Instant from, final Instant to) {
+                return (int) Duration.between(from, to).toHours() * 100;
+            }
+        };
+    }
 
     @Test
     public void euroCents_1h_1eur() {
         final Instant now = Instant.now(),
-                h1 = now.plusSeconds(3600);
-        assertEquals(100, pricing.euroCents(h1));
+                h1 = now.minusSeconds(3600);
+        assertEquals(100, pricing.euroCents(h1, now));
     }
 }
